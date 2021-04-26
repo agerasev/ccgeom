e916a93635fd7142264a54bd0d08a44248a9d331
Alexey Gerasev <alexey.gerasev@gmail.com>
2021-04-26 09:59:29 +0700

    Normal transformation

--- src/map.rs
+++ src/map.rs
@@ -1,17 +1,17 @@
-use vecmat::{Transform, traits::Normalize};
+use vecmat::{Transform, transform::Directional, traits::Normalize};
 
 pub trait Map<P, D = P> {
     fn identity() -> Self;
 
     fn apply_pos(&self, pos: P) -> P;
     fn apply_dir(&self, pos: P, dir: D) -> D;
+    fn apply_normal(&self, pos: P, normal: D) -> D;
     
     fn chain(self, other: Self) -> Self;
-    
     fn inv(self) -> Self;
 }
 
-impl<A, T> Map<T, T> for A where A: Transform<T>, T: Normalize {
+impl<A, T> Map<T, T> for A where A: Directional<T>, T: Normalize {
     fn identity() -> Self {
         <A as Transform<T>>::identity()
     }
@@ -20,13 +20,15 @@ impl<A, T> Map<T, T> for A where A: Transform<T>, T: Normalize {
         <A as Transform<T>>::apply(self, pos)
     }
     fn apply_dir(&self, pos: T, dir: T) -> T {
-        <A as Transform<T>>::deriv(self, pos, dir).normalize()
+        <A as Directional<T>>::apply_dir(self, pos, dir)
+    }
+    fn apply_normal(&self, pos: T, dir: T) -> T {
+        <A as Directional<T>>::apply_normal(self, pos, dir)
     }
 
     fn chain(self, other: Self) -> Self {
         <A as Transform<T>>::chain(self, other)
     }
-
     fn inv(self) -> Self {
         <A as Transform<T>>::inv(self)
     }
